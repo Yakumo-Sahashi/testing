50027f9fffe33d3733a16558d9befe13816666e0
Yakumo-Sahashi <Diego.Bollas@hotmail.com>
2024-05-06 13:52:20 -0600

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php require_once "./php/dependencias.php" ?>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col"></div>
+      <div class="col">
+        <?php include_once "./view/login.php" ?>
+      </div>
+      <div class="col"></div>
+    </div>
+</div>
+</body>
+</html>
---
+++ php/dependencias.php
@@ -0,0 +1,6 @@
+<!-- dependencias visuales -->
+<link rel="stylesheet" href="./assets/css/bootstrap.css">
+
+<!-- dependencias de funcionalidad -->
+<script src="./assets/js/jquery.js"></script>
+<script src="./assets/js/bootstrap.js"></script>
