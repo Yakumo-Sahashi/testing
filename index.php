3bcde11bc3f09e03c35e9186f5eb9b0239dddd71
Yakumo-Sahashi <Diego.Bollas@hotmail.com>
2024-05-06 14:41:42 -0600

    cambios en la rama main

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <div class="row">
       <div class="col"></div>
       <div class="col">
-        <?php include_once "./view/login.php" ?>
+        <h1>estoy en la rama principal</h1>
       </div>
       <div class="col"></div>
     </div>
